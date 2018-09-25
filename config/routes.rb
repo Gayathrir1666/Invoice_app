adc52b588307aa04590ca778e014b5e5a218b7c0
Gayathrir1666 <gayathrir9911@gmail.com>
2018-09-25 15:00:42 +0530

    Vendor connects to stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :vendors, controlllers: { registrations: "registrations"}
+  get 'omniauth_callbacks/stripe_connect'
+  devise_for :vendors, controllers: { registrations: "registrations", omniauth_callbacks: "omniauth_callbacks"}
   get "connect-to-stripe", to: "vendor_onboarding#connect_to_stripe"
   root "home#index"
 end
